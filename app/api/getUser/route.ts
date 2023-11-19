213a098ef88109d7000ac8edd5eb52236cee4b24
kasi_sj <jkasinathan007@gmail.com>
2023-11-19 13:32:49 +0530

    update

--- app/api/getUser/route.ts
+++ app/api/getUser/route.ts
@@ -1,7 +1,16 @@
 import { connectTodDB } from "@/utils/database";
 import User from "@/models/user";
+import { getServerSession } from "next-auth";
 export const POST = async(req : any)=>{
     const {email} : any = await req.json();
+    const session = await getServerSession();
+    if(!session||!session.user||!session.user.email){
+        return new Response(JSON.stringify({message:"you are not allowed to access this data"}));
+    }
+    const sessionemail  = session.user.email ;
+    if(email !== sessionemail){
+        return new Response(JSON.stringify({message:"you are not allowed to access this data"}));
+    }
     try{
         await connectTodDB();
         const res = await User.findOne({email});
